585f21bf8befa8132f0d01584473b9ba3728d593
OGOLA-SOSPETER <captainsos483@gmail.com>
2023-03-22 09:21:17 +0000

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,18 @@
-#A python script to return list of lists of pascal's triangle
+#the pascal triangle
 
 def pascal_triangle(n):
+    triangle = [[1]]
     if n <= 0:
         return []
-    triangle = [[1]]
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]
-        for j in range(1, i):
-            new_row.append(prev_row[j-1] + prev_row[j])
-        new_row.append(1)
-        triangle.append(new_row)
-    return triangle
+    else:
+        for count in range(1,n):
+            previous = triangle[-1]
+            next = [1]
+            for counter in range(1,count):
+                next.append(previous[counter-1] + previous[counter])
+            next.append(1)
+            triangle.append(next)
+        for row in range(len(triangle)):
+            print(triangle[row])
+        return triangle
+
