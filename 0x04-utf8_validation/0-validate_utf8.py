5f646d1e3f82df2699eef4618508f0ccf6635484
OGOLA-SOSPETER <captainsos483@gmail.com>
2023-04-20 21:09:13 +0000

    a method that determines if a given data set represents a valid UTF-8 encoding.

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,44 +3,27 @@
 UTF-8 Validation
 """
 
-def validUTF8(data):
-    # Initialize a count of the number of bytes in the current UTF-8 character
-    num_bytes = 0
 
-    # Iterate over each byte in the data set
-    for byte in data:
-        # Check if this byte is the start of a new UTF-8 character
-        if num_bytes == 0:
-            # Determine the number of bytes in this UTF-8 character
-            if (byte >> 7) == 0b0:
-                # This is a single-byte UTF-8 character
-                num_bytes = 1
-            elif (byte >> 5) == 0b110:
-                # This is a two-byte UTF-8 character
-                num_bytes = 2
-            elif (byte >> 4) == 0b1110:
-                # This is a three-byte UTF-8 character
-                num_bytes = 3
-            elif (byte >> 3) == 0b11110:
-                # This is a four-byte UTF-8 character
-                num_bytes = 4
-            else:
-                # This byte is not the start of a valid UTF-8 character
+def validUTF8(data):
+    """
+    data: a list of integers
+    Return: True if data is a valid UTF-8
+    encoding, else return False
+    """
+    byte_count = 0
+
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
                 return False
         else:
-            # This byte is part of a multi-byte UTF-8 character
-            if (byte >> 6) != 0b10:
-                # This byte is not a continuation byte
+            if i >> 6 != 0b10:
                 return False
-            
-            # Decrement the count of bytes remaining in the current UTF-8 character
-            num_bytes -= 1
-        
-    # If we reach the end of the data set and there are still bytes remaining in the current UTF-8 character,
-    # then the data set is not a valid UTF-8 encoding
-    if num_bytes > 0:
-        return False
-    
-    # Otherwise, the data set is a valid UTF-8 encoding
-    return True
-
+            byte_count -= 1
+    return byte_count == 0
